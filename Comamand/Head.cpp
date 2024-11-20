cdd254c60c5a59e11210eed91317fd88afdc2aaf
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2024-11-21 00:21:04 +0100

    head

--- Comamand/Head.cpp
+++ Comamand/Head.cpp
@@ -1,7 +1,23 @@
 #include "Head.h"
 
+#include <regex>
+
 Head::~Head(){}
 
 void Head::execute(const string& args, string& input){
+    regex rgx("-n(\\d{1,5})\\s(.*)");
 
+    smatch matches;
+    if (regex_search(args, matches, rgx)) {
+        int n = stoi(matches[1]);  // Captured number (1 to 5 digits)
+        reader = new FileReader(matches[2]);
+
+        while (!reader->endOfRead() && n > 0) {
+            cout << reader->getNextLine() << endl;
+            n--;
+        }
+    }
+    else {
+        cout << "Error" << endl;
+    }
 }

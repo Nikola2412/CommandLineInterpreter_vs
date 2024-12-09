d6834324da7ac1ef183ccc3c98e57647063e09d5
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2024-12-09 01:30:25 +0100

    nmp

--- Comamand/Tr.cpp
+++ Comamand/Tr.cpp
@@ -5,6 +5,9 @@ Tr::~Tr(){}
 
 void Tr::execute(const string& args){
 	this->set(args);
-	std::replace(this->Argument().begin(), this->Argument().end(), ':', '.');
+	int n = args.size();
+	char c1 = args[n - 6];
+	char c2 = args[n - 2];
+	std::replace(this->Argument().begin(), this->Argument().end(), c1, c2);
 	writer->writeLine(this->Argument());
 }
